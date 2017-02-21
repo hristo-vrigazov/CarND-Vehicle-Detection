f9a80c825341f7a2c0dca8874829380408097a1b
hristo-vrigazov <hvrigazov@gmail.com>
2017-02-21 10:33:25 +0200

    Added function for training

--- car_not_car_classifier.py
+++ car_not_car_classifier.py
@@ -1,14 +1,19 @@
 from imutils import paths
 from utils import extract_features
 from utils import save_array, load_array
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
 
 import numpy as np
 import matplotlib.image as mpimg
 import os
+import time
+
 
 class CarNotCarClassifier:
 
-	def __init__(self, 
+	def load_data(self, 
 		serialized_data_folder='./',
 		path_to_vehicle_folder='vehicles/', 
 		path_to_non_vehicle_folder='non-vehicles/'):
@@ -36,10 +41,22 @@ class CarNotCarClassifier:
 			save_array(path_to_y_dat_file, self.y)
 			print('Done saving arrays')
 
-		
+
+	def train(self):
+		X_scaler = StandardScaler().fit(self.X)
+		scaled_X = X_scaler.transform(self.X)
+		X_train, X_test, y_train, y_test = train_test_split(
+			scaled_X, self.y, test_size=0.2, random_state=7)
+
+		svc = LinearSVC()
+		print('Start fitting')
+		start_time = time.time()
+		svc.fit(X_train, y_train)
+		print('Time for training: ', time.time() - start_time, ' seconds')
+		print('Train Accuracy of SVC = ', svc.score(X_train, y_train))
+		print('Test Accuracy of SVC = ', svc.score(X_test, y_test))
 
 	def _get_X_y(self, path_to_images, class_label):
-		X = extract_features(paths.list_images(path_to_images))
+		X = np.array(extract_features(paths.list_images(path_to_images))).astype(np.float64)
 		y = np.full(len(X), class_label)
 		return X, y
-
